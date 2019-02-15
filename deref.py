54d19d4b8384f345ad739b41094cff4a0d50d4fa
David Ojeda <david@omind.me>
2019-02-15 11:20:21 +0100

    Add minor fix on deref script

--- deref.py
+++ deref.py
@@ -5,16 +5,14 @@ import jsonref
 
 
 @click.command()
-@click.argument('input_yaml')
-@click.argument('output_json')
-def cli(input_yaml, output_json):
+@click.argument('input_json', type=click.File(mode='r'))
+@click.argument('output_json', type=click.File(mode='w'))
+def cli(input_json, output_json):
     """Convert a openapi yaml specification to json without $ref references"""
-    with open(input_yaml, 'r') as fin:
-        api = json.load(fin)
+    api = json.load(input_json)
 
     api_noref = jsonref.JsonRef.replace_refs(api)
-    with open(output_json, 'w') as fout:
-        json.dump(api_noref, fout, indent=4)
+    json.dump(api_noref, output_json, indent=4)
 
 
 if __name__ == '__main__':
