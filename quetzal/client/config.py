a4cadffd2f9905b1812593c55989f54432db6c1b
David Ojeda <david.ojeda@gmail.com>
2019-03-08 12:22:29 +0100

    Add default values from environment variables

--- quetzal/client/config.py
+++ quetzal/client/config.py
@@ -1,9 +1,16 @@
+import os
+
 import quetzal.openapi_client.configuration
 
 
 class Configuration(quetzal.openapi_client.configuration.Configuration):
     # Use this later for particular extensions/modifications on the API
     # configuration object
-    pass
 
+    def __init__(self):
+        super().__init__()
+        if 'QUETZAL_URL' in os.environ:
+            self.host = os.getenv('QUETZAL_URL', '')
+        self.username = os.getenv('QUETZAL_USER', '')
+        self.password = os.getenv('QUETZAL_PASSWORD', '')
 
